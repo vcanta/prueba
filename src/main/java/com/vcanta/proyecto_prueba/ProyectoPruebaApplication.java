04e25326375ad57105bc5b9bfbdfd514abd3c269
Victor Canta <vacpcanta@gmail.com>
2017-07-30 09:15:48 -0500

    Contenido de Proyecto de prueba

---
+++ src/main/java/com/vcanta/proyecto_prueba/ProyectoPruebaApplication.java
@@ -0,0 +1,12 @@
+package com.vcanta.proyecto_prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPruebaApplication.class, args);
+	}
+}
